765a1d2fd4c06a298a98f8fb80f667d927ced2f8
kash m <40723979-demo12345612e@users.noreply.replit.com>
2025-03-21 20:01:28 +0000

    Restored to 'b02f97cafd88cf3aede9e746aa4449e692e3d59c'
    Replit-Restored-To: b02f97cafd88cf3aede9e746aa4449e692e3d59c

--- utils/visualization.py
+++ utils/visualization.py
@@ -565,8 +565,6 @@ def create_3d_asteroid_visualization(df, map_type='3d'):
 
 def create_3d_asteroid_visualization(df, map_type='3d'):
     """
-    def create_3d_visualization(df):
-    """
     Create a 3D visualization of asteroids
     
     Parameters:
@@ -578,7 +576,6 @@ def create_3d_asteroid_visualization(df, map_type='3d'):
     --------
     plotly.graph_objects.Figure
         Plotly figure object
-    """
     """
     # Create a 3D scatter plot
     fig = go.Figure()
