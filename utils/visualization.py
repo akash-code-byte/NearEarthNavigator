b268e03ce6a91d7e63ed4df3fedc038e1b883563
kash m <40723979-demo12345612e@users.noreply.replit.com>
2025-03-21 23:47:23 +0000

    Restored to '2a930b14fcf6b77dd03205964df92d172f6aca82'
    Replit-Restored-To: 2a930b14fcf6b77dd03205964df92d172f6aca82

--- utils/visualization.py
+++ utils/visualization.py
@@ -8,7 +8,6 @@ def plot_asteroids(df):
     """
     Create a scatter plot of asteroids showing miss distance vs. relative velocity
     
-    """
     Parameters:
     -----------
     df : pandas.DataFrame
